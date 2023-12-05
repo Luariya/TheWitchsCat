3df0249b72f736d2cc32ee6505538193897be480
Nelly <luariya1212@gmail.com>
2023-12-05 10:31:55 +0100

    kleine changes

--- The Witch's Cat/Assets/Scripts/Inventory/InventoryItemBase.cs
+++ The Witch's Cat/Assets/Scripts/Inventory/InventoryItemBase.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Rucksack : MonoBehaviour
+public class InventoryItemBase : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
